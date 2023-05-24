66d16493171df5ec39826d754918bc84cb7c0479
unknown <clsandoval@up.edu.ph>
2023-05-24 14:41:00 +0800

    add date sending

--- bot.py
+++ bot.py
@@ -5,13 +5,19 @@ from functions import *
 json_data = query(days=2)
 matches = [i["match_id"] for i in json_data["rows"]]
 durations = [i["duration"] for i in json_data["rows"]]
+dates = [
+    datetime.utcfromtimestamp(int(i["start_time"])).strftime("%Y-%m-%d %H:%M:%S")
+    for i in json_data["rows"]
+]
 # %%
-for match_id, duration in zip(matches, durations):
+for match_id, duration, date in zip(matches, durations, dates):
     match_data = query_stratz(match_id)
     total_kills, kill_density = ret_kill_density(match_data, duration)
-    if kill_density != -1:
-        message = "Match: opendota.com/matches/{}\nDuration: {}\nTotal Kills: {}\nKill Density: {}".format(
-            match_id, duration, total_kills, kill_density
+    if kill_density != -1 and kill_density > 1.5:
+        message = "{}\nMatch: opendota.com/matches/{}\nDuration: {}\nTotal Kills: {}\nKill Density: {}".format(
+            date, match_id, duration, total_kills, kill_density
         )
         send_message(message)
     time.sleep(0.5)
+
+# %%
