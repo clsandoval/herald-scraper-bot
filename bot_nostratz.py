96550520a8f6c8f151e147bc6eb145469556a6f6
clsandoval <clsandoval@up.edu.ph>
2023-08-22 13:36:02 +0800

    Add Herald Match Scraping functionality

---
+++ bot_nostratz.py
@@ -0,0 +1,20 @@
+from functions import *
+import logging, time
+
+logging.info("Start Herald Match Scraping")
+json_data = query(days=7)
+logging.info("Opendota Data Pulled")
+matches = [i["match_id"] for i in json_data["rows"]]
+durations = [i["duration"] for i in json_data["rows"]]
+dates = [
+    datetime.utcfromtimestamp(int(i["start_time"])).strftime("%Y-%m-%d %H:%M:%S")
+    for i in json_data["rows"]
+]
+
+for match, duration, date in zip(matches, durations, dates):
+    message = "{}\nMatch: opendota.com/matches/{}\nDuration: {}".format(
+        date, match, duration
+    )
+    send_message(message)
+    time.sleep(0.5)
+logging.info("Scrape complete")
